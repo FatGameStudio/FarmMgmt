e999d5c3ae5c7326659c8fee55de3b4294d76867
tof <christophe.lebegue@gmail.com>
2018-01-28 14:52:55 +0100

    SpringBoot init

---
+++ src/main/java/no/lebegue/christophe/FarmMgmt/FarmMgmtApplication.java
@@ -0,0 +1,12 @@
+package no.lebegue.christophe.FarmMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmMgmtApplication.class, args);
+	}
+}
---
+++ src/test/java/no/lebegue/christophe/FarmMgmt/FarmMgmtApplicationTests.java
@@ -0,0 +1,16 @@
+package no.lebegue.christophe.FarmMgmt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FarmMgmtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

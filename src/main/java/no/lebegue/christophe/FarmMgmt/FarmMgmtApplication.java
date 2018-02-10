5349f40d16748df2079d679a166a580ae51e50f5
tof <christophe.lebegue@gmail.com>
2018-02-10 19:20:04 +0100

    pb with tomcat deploy

--- src/main/java/no/lebegue/christophe/FarmMgmt/FarmMgmtApplication.java
+++ src/main/java/no/lebegue/christophe/FarmMgmt/FarmMgmtApplication.java
@@ -2,11 +2,17 @@ package no.lebegue.christophe.FarmMgmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class FarmMgmtApplication {
+public class FarmMgmtApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FarmMgmtApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(FarmMgmtApplication.class, args);
 	}

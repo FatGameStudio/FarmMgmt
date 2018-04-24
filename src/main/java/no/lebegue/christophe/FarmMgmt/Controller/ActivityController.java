54d717ae1335c61f15bee482d671ba4e011de95a
tof <christophe.lebegue@gmail.com>
2018-04-24 14:18:52 +0200

    remove useless import

--- src/main/java/no/lebegue/christophe/FarmMgmt/Controller/ActivityController.java
+++ src/main/java/no/lebegue/christophe/FarmMgmt/Controller/ActivityController.java
@@ -9,10 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import no.lebegue.christophe.FarmMgmt.Entity.Activity;
 import no.lebegue.christophe.FarmMgmt.Entity.ActivityRepository;
